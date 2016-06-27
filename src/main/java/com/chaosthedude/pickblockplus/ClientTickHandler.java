20e9ca0c566faa0fba3eb5d1245e2f8ea40617ab
ChaosTheDude <chaosthedude@gmail.com>
2016-06-27 00:32:36 -0400

    Initial commit

---
+++ src/main/java/com/chaosthedude/pickblockplus/ClientTickHandler.java
@@ -0,0 +1,181 @@
+package com.chaosthedude.pickblockplus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.enchantment.Enchantment.Rarity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+public class ClientTickHandler {
+
+	private final Minecraft mc = Minecraft.getMinecraft();
+	private ItemStack[] hotbar = new ItemStack[9];
+	private boolean activated = true;
+	private int ticksSincePressed = 0;
+
+	@SubscribeEvent
+	public void onClientTick(TickEvent.ClientTickEvent event) {
+		if (event.phase != TickEvent.Phase.START || event.type != TickEvent.Type.CLIENT) {
+			return;
+		}
+
+		overrideVanillaPickBlock();
+		checkPickBlockKey();
+	}
+
+	private void checkPickBlockKey() {
+		EntityPlayer player = mc.thePlayer;
+		if (player == null) {
+			return;
+		}
+
+		if (!PickBlockPlus.pickBlockPlus.isKeyDown()) {
+			activated = false;
+			ticksSincePressed++;
+			if (ticksSincePressed > 20) {
+				ticksSincePressed = 20;
+			}
+
+			return;
+		}
+
+		if (activated) {
+			return;
+		}
+
+		activated = true;
+		if (mc.currentScreen != null) {
+			return;
+		}
+
+		RayTraceResult target = mc.objectMouseOver;
+		if (target == null) {
+			return;
+		}
+
+		if (player.capabilities.isCreativeMode) {
+			ForgeHooks.onPickBlock(target, player, mc.theWorld);
+			mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+			return;
+		}
+
+		int slot = player.inventory.currentItem;
+		if (hotbar.length != InventoryPlayer.getHotbarSize()) {
+			hotbar = new ItemStack[InventoryPlayer.getHotbarSize()];
+		}
+
+		if (ticksSincePressed > 10) {
+			ticksSincePressed = 0;
+			List<ItemStack> validItems = new ArrayList();
+			if (slot >= 0 && 0 < hotbar.length && hotbar[slot] != null) {
+				ItemStack original = hotbar[slot];
+				hotbar[slot] = null;
+				boolean moved = false;
+				for (int i = 0; i < InventoryPlayer.getHotbarSize(); i++) {
+					if (Util.areItemStacksIdentical(original, player.inventory.getStackInSlot(i))) {
+						moved = true;
+						break;
+					}
+				}
+
+				if (!moved) {
+					validItems.add(original);
+				}
+			}
+
+			if (target.typeOfHit == RayTraceResult.Type.BLOCK) {
+				World world = player.worldObj;
+				BlockPos pos = target.getBlockPos();
+				IBlockState state = player.worldObj.getBlockState(pos);
+
+				validItems.add(world.getBlockState(pos).getBlock().getPickBlock(state, target, world, pos, player));
+				validItems.add(Util.getBrokenBlock(world, pos));
+				validItems.add(new ItemStack(world.getBlockState(pos).getBlock(), 1));
+			} else if (target.typeOfHit == RayTraceResult.Type.ENTITY) {
+				validItems.add(target.entityHit.getPickedResult(target));
+			}
+
+			ItemStack held = player.getHeldItem(EnumHand.MAIN_HAND);
+			for (ItemStack stack : validItems) {
+				for (int invSlot = 0; invSlot < player.inventory.mainInventory.length; invSlot++) {
+					if (stack != null) {
+						ItemStack possibleItem = player.inventory.mainInventory[invSlot];
+						if (possibleItem != null) {
+							if (possibleItem.isItemEqual(stack)) {
+								mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+								if (invSlot < 9) {
+									player.inventory.currentItem = invSlot;
+									return;
+								}
+
+								Util.swapItems(player, held, invSlot, hotbar);
+								return;
+							}
+						}
+					}
+				}
+			}
+		} else {
+			ticksSincePressed = 0;
+			boolean targetIsEntity = false;
+			IBlockState state = null;
+			if (target.typeOfHit == RayTraceResult.Type.BLOCK) {
+				state = player.worldObj.getBlockState(target.getBlockPos());
+			} else if (target.typeOfHit == RayTraceResult.Type.ENTITY) {
+				targetIsEntity = true;
+			}
+
+			if (!targetIsEntity && state == null) {
+				return;
+			}
+
+			ItemStack held = player.getHeldItem(EnumHand.MAIN_HAND);
+			int bestSlot = -1;
+			if (targetIsEntity) {
+				bestSlot = Util.getHighestDamageItemSlot(player);
+			} else {
+				bestSlot = Util.getMostEffectiveItemSlot(player, state);
+			}
+			
+			if (bestSlot == -1) {
+				return;
+			}
+
+			mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+			if (bestSlot < 9) {
+				player.inventory.currentItem = bestSlot;
+				return;
+			}
+
+			Util.swapItems(player, held, bestSlot, hotbar);
+			return;
+		}
+	}
+
+	private void overrideVanillaPickBlock() {
+		GameSettings settings = Minecraft.getMinecraft().gameSettings;
+		if (settings.keyBindPickBlock.getKeyCode() != 0 && PickBlockPlus.pickBlockPlus.getKeyCode() == 0) {
+			PickBlockPlus.logger.info("Overriding vanilla pick block");
+			settings.setOptionKeyBinding(PickBlockPlus.pickBlockPlus, settings.keyBindPickBlock.getKeyCode());
+			settings.setOptionKeyBinding(settings.keyBindPickBlock, 0);
+			KeyBinding.resetKeyBindingArrayAndHash();
+		}
+	}
+
+}
---
+++ src/main/java/com/chaosthedude/pickblockplus/Util.java
@@ -0,0 +1,184 @@
+package com.chaosthedude.pickblockplus;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Multimap;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class Util {
+
+	public static boolean areItemStacksIdentical(ItemStack a, ItemStack b) {
+		if (a == null && b == null) {
+			return true;
+		}
+
+		if (a == null || b == null) {
+			return false;
+		}
+
+		return canItemStacksMerge(a, b);
+	}
+
+	public static boolean canItemStacksMerge(ItemStack a, ItemStack b) {
+		if (a == null || b == null) {
+			return true;
+		}
+
+		return a.getItemDamage() == b.getItemDamage() && a.getItem() == b.getItem()
+				&& ItemStack.areItemStackTagsEqual(a, b);
+	}
+
+	public static ItemStack getBrokenBlock(World world, BlockPos pos) {
+		Block block = world.getBlockState(pos).getBlock();
+		if (block == null) {
+			return null;
+		}
+
+		List<ItemStack> dropped = block.getDrops(world, pos, world.getBlockState(pos), 0);
+		if (dropped == null || dropped.isEmpty()) {
+			return null;
+		}
+
+		ItemStack main = (ItemStack) dropped.remove(0);
+		for (int i = 0; i < dropped.size(); i++) {
+			ItemStack other = (ItemStack) dropped.get(i);
+			if (!canItemStacksMerge(main, other)) {
+				return null;
+			}
+
+			main.stackSize += other.stackSize;
+		}
+
+		return main;
+	}
+	
+	public static int getMostEffectiveItemSlot(EntityPlayer player, IBlockState state) {
+		List<Integer> possibleItems = new ArrayList();
+		for (int invSlot = 0; invSlot < player.inventory.mainInventory.length; invSlot++) {
+			ItemStack possibleItem = player.inventory.mainInventory[invSlot];
+			if (possibleItem != null) {
+				Set<String> toolClasses = possibleItem.getItem().getToolClasses(possibleItem);
+				for (String toolClass : toolClasses) {
+					if (state.getBlock().isToolEffective(toolClass, state)) {
+						possibleItems.add(invSlot);
+					} else if (state.getBlock().getHarvestLevel(state) == -1) {
+						if (state.getBlock().getMaterial(state) == Material.ROCK && toolClasses.contains("pickaxe")) {
+							possibleItems.add(invSlot);
+						}
+					}
+				}
+			}
+		}
+
+		int bestSlot = -1;
+		for (int invSlot : possibleItems) {
+			ItemStack stack = player.inventory.mainInventory[invSlot];
+			if (stack != null) {
+				if (bestSlot == -1) {
+					bestSlot = invSlot;
+				} else {
+					ItemStack bestStack = player.inventory.mainInventory[bestSlot];
+					Item possibleTool = stack.getItem();
+					Item bestTool = bestStack.getItem();
+					if (stack.getStrVsBlock(state) > bestStack.getStrVsBlock(state)) {
+						bestSlot = invSlot;
+					}
+				}
+			}
+		}
+		
+		return bestSlot;
+	}
+
+	public static int getHighestDamageItemSlot(EntityPlayer player) {
+		int highestDamageSlot = -1;
+		double highestDamage = -1D;
+		double highestSpeed = -1D;
+		for (int invSlot = 0; invSlot < player.inventory.mainInventory.length; invSlot++) {
+			ItemStack stack = player.inventory.mainInventory[invSlot];
+			if (stack != null) {
+				if (highestDamageSlot == -1) {
+					highestDamageSlot = invSlot;
+				} else {
+					double damage = -1D;
+					Multimap map = stack.getAttributeModifiers(EntityEquipmentSlot.MAINHAND);
+					Collection collection = map.get(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName());
+					for (Object o : collection) {
+						if (o instanceof AttributeModifier) {
+							AttributeModifier modifier = (AttributeModifier) o;
+							if (modifier.getName().equals("Weapon modifier") || modifier.getName().equals("generic.attackDamage") || modifier.getName().equals("Tool modifier")) {
+								damage = modifier.getAmount();
+							}
+						}
+					}
+
+					if (damage > highestDamage) {
+						highestDamage = damage;
+						highestDamageSlot = invSlot;
+					} else if (damage == highestDamage) {
+						double speed = -1D;
+						Multimap map1 = stack.getAttributeModifiers(EntityEquipmentSlot.MAINHAND);
+						Collection collection1 = map1.get(SharedMonsterAttributes.ATTACK_SPEED.getAttributeUnlocalizedName());
+						for (Object o : collection1) {
+							if (o instanceof AttributeModifier) {
+								AttributeModifier modifier = (AttributeModifier) o;
+								if (modifier.getName().equals("Weapon modifier") || modifier.getName().equals("Tool modifier")) {
+									speed = modifier.getAmount();
+								}
+							}
+						}
+						
+						if (speed < highestSpeed) {
+							highestSpeed = speed;
+							highestDamageSlot = invSlot;
+						}
+					}
+				}
+			}
+		}
+
+		return highestDamageSlot;
+	}
+
+	public static void swapItems(EntityPlayer player, ItemStack held, int invSlot, ItemStack[] hotbar) {
+		int targetSlot = player.inventory.currentItem;
+		Minecraft.getMinecraft().playerController.pickItem(invSlot);
+		if (held == null) {
+			return;
+		}
+
+		if (hotbar[targetSlot] == null) {
+			hotbar[targetSlot] = held;
+			return;
+		}
+
+		boolean canReplace = false;
+		for (int barSlot = 0; barSlot < 9; barSlot++) {
+			ItemStack barItem = player.inventory.getStackInSlot(barSlot);
+			if (barItem != null && barItem == hotbar[targetSlot]) {
+				canReplace = true;
+				break;
+			}
+		}
+
+		if (canReplace) {
+			hotbar[targetSlot] = held;
+		}
+	}
+
+}
