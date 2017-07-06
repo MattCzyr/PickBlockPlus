717429430b646c52a29801d29f8714acd3ec59cc
mattczyr <mattczyr@gmail.com>
2017-07-06 17:31:52 -0400

    Update to 1.12 (Forge #2397)

--- src/main/java/com/chaosthedude/pickblockplus/PickBlockPlus.java
+++ src/main/java/com/chaosthedude/pickblockplus/PickBlockPlus.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = PickBlockPlus.MODID, name = PickBlockPlus.NAME, version = PickBlockPlus.VERSION, acceptedMinecraftVersions = "[1.11.2]", clientSideOnly = true)
+@Mod(modid = PickBlockPlus.MODID, name = PickBlockPlus.NAME, version = PickBlockPlus.VERSION, acceptedMinecraftVersions = "[1.12]", clientSideOnly = true)
 
 public class PickBlockPlus {
 
